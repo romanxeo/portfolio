cbe0ffdcc6c368a1b4fa54751add9bcceec2aa04
romanxeo <romanxeo@gmail.com>
2021-07-27 22:02:01 +0300

    Portfolio 1

---
+++ src/Header/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import s from './Header.module.css';
+
+function Header() {
+    return (
+        <div>
+            HEADER
+        </div>
+    );
+}
+
+export default Header;
